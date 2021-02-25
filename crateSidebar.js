d9f3bef76ed33c7eaaff08dd97b382eacbc610e8
ibarkay <ibarkay@gmail.com>
2021-02-25 15:21:58 +0200

    starting - pics, art

--- crateSidebar.js
+++ crateSidebar.js
@@ -6,5 +6,15 @@ for (let i = 0; i < 3; i++) {
   div.classList.add('tool');
   div.setAttribute('data-type', `t${i}`);
   div.innerHTML = i;
+  const span = document.createElement('span');
+  span.classList.add('tooltiptext');
+  if (i === 0) {
+    span.innerHTML = 'Shovel - dirt & grass';
+  } else if (i === 1) {
+    span.innerHTML = 'Axe - wood';
+  } else {
+    span.innerHTML = 'Pickaxe - stone';
+  }
+  div.appendChild(span);
   tools.appendChild(div);
 }
