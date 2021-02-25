7b0c95df72dbcf38d9759513bd3bca8ce49cfe45
ibarkay <ibarkay@gmail.com>
2021-02-25 15:57:41 +0200

    starting - pics, art

--- createInventory.js
+++ createInventory.js
@@ -1,10 +1,26 @@
 const inventory = document.querySelector('#inventory');
-const inventorySum = document.createElement('h3');
-inventorySum.innerHTML = `wood : ${wood}\n
-stones : ${stones}\n
-dirts: ${dirts}
-grass: ${grass}`;
+// const inventorySum = document.createElement('h3');
+// inventorySum.innerHTML = `wood : ${wood}\n
+// stones : ${stones}\n
+// dirts: ${dirts}
+// grass: ${grass}`;
 
+// // refresh inventory function
+// const tick = () => {
+//   inventorySum.innerHTML = `wood : ${wood}\n
+// stones : ${stones}\n
+// dirts: ${dirts}
+// grass: ${grass}`;
+// };
+
+// inventory.append(inventorySum);
+const inventorySum = document.createElement('div');
+inventory.setAttribute('id', 'matiriales-container');
+for (let i = 0; i < 4; i++) {
+  const boxy = document.createElement('div');
+  boxy.setAttribute('id', `mat-${i}`);
+  inventory.append(boxy);
+}
 // refresh inventory function
 const tick = () => {
   inventorySum.innerHTML = `wood : ${wood}\n
@@ -12,6 +28,5 @@ stones : ${stones}\n
 dirts: ${dirts}
 grass: ${grass}`;
 };
-
 
 inventory.append(inventorySum);
